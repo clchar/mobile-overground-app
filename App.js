adabd6072deea7b38616cd56702bc24f7fc76f42
Charikleia Charalampidou <cl.charal@gmail.com>
2020-05-10 20:19:13 +0100

    Fetches data from tfl api and formats date

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Arrivals } from "./src/components/Arrivals";
+import { Header } from "./src/components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <Arrivals />
     </View>
   );
 }
@@ -12,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
   }
 });
