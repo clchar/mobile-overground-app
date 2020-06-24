1c0bbfad938d7ce8c61f465ae4e5386af94ba696
Charikleia Charalampidou <cl.charal@gmail.com>
2020-06-24 18:17:49 +0100

    Add details in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
 import TFLLOGO from "../assets/tfl-logo.png";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#f2f2f2",
     alignItems: "center",
-    padding: 20
+    padding: 50
   },
   logo: {
     width: 100,
@@ -17,6 +18,8 @@ const styles = StyleSheet.create({
     fontSize: 20
   },
   destinationWrapper: {
+    display: "flex",
+    alignItems: "center",
     marginTop: 10
   },
   destinationText: {
@@ -29,10 +32,13 @@ export const Header = () => {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={TFLLOGO} />
-      <Text style={styles.text}>Overground Arrivals</Text>
+      <Text style={styles.text}>Overground</Text>
 
       <View style={styles.destinationWrapper}>
+        <Text>From</Text>
         <Text style={styles.destinationText}>Caledonian Road & Barnsbury</Text>
+        <Text>To</Text>
+        <Text style={styles.destinationText}>Stratford</Text>
       </View>
     </View>
   );
